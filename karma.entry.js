cf027fb496f619531c89fadfc110c17292e41ecb
Alessandro Palombaro <alexpalombaro@gmail.com>
2015-10-14 09:27:06 +0800

    Fix karma runner tests

--- karma.entry.js
+++ karma.entry.js
@@ -1,4 +1,4 @@
-// Require all ".spec.js" files in ~/client.
-var context = require.context('./client', true, /.+\.spec\.js$/);
+// Require all ".spec.js" files in ~/tests.
+var context = require.context('./tests', true, /.+\.spec\.js$/);
 context.keys().forEach(context);
 module.exports = context;
